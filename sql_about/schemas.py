f1a8a9d0a7d1c09914614598ea5fae63980e674a
noname2048 <sungwook.csw@gmail.com>
2021-07-29 22:32:43 +0900

    fix(schema): follows change of model

--- sql_about/schemas.py
+++ sql_about/schemas.py
@@ -6,26 +6,34 @@ from pydantic import BaseModel
 
 #### RecevieData Base + CRUD ####
 class ReceiveDataBase(BaseModel):
-    time: datetime
+    device_id: int
+    timestamp: datetime
+
+    class Config:
+        orm_mode = True
 
 class ReceiveDataCreate(ReceiveDataBase):
     pass
 
 class RecevieData(ReceiveDataBase):
+    id: int
+    is_active: bool
+    warn_at_inactive: bool
 
     class Config:
         orm_mode = True
 
 #### Device Base + CRUD ####
 class DeviceBase(BaseModel):
-    title: str
-    description: Optional[str] = None
+    device_id : int
+    name: str
 
 class DeviceCreate(DeviceBase):
     pass
 
 class Device(DeviceBase):
     id: int
+    device_id: int
     name: str
     receivedatas: List[RecevieData] = []
 
